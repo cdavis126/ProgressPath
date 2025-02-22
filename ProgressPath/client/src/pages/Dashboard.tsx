90834d03b54ab346a85dc04cdb9c0d087261988b
Celeste Hayes <celestehayes@Mac.attlocal.net>
2025-02-21 23:06:30 -0500

    Style: updates to the dashboard page

--- ProgressPath/client/src/pages/Dashboard.tsx
+++ ProgressPath/client/src/pages/Dashboard.tsx
@@ -1,18 +1,33 @@
 import Navbar from '../components/Navbar';
 import Header from '../components/Header';
+import { AiOutlinePlus } from 'react-icons/ai';
 
 const Dashboard = () => {
+  const handleClick = () => {
+    console.log("Clicked the + icon!");
+  };
+
   return (
     <div>
       <Navbar />
-	    <Header />
+      <Header />
+      <div style={{ marginTop: '50px', paddingLeft: '30px', display: 'flex', alignItems: 'center' }}>
+        <h1 style={{ marginRight: '10px' }}>Create Your Path</h1>
+        <AiOutlinePlus 
+          onClick={handleClick} 
+          style={{
+            cursor: 'pointer', 
+            fontSize: '1.5rem', 
+            color: '#6c5ce7'
+          }} 
+        />
+      </div>
       <div style={{ marginTop: '50px', paddingLeft: '30px' }}>
-        <h1>Path Section</h1>
-        <p>This is the path/goal section placeholder</p>
+        <h1>Follow Your Path</h1>
+        <p>This is the path/goal tracker section placeholder</p>
       </div>
     </div>
   );
 };
 
 export default Dashboard;
-
