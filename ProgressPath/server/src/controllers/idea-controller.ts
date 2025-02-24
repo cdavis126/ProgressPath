b6a4718fdfe583253ac7137880fa22ef4086db69
Celeste Hayes <celestehayes@Mac.attlocal.net>
2025-02-23 19:24:13 -0500

    Update idea-controller and package.json to get server running

--- ProgressPath/server/src/controllers/idea-controller.ts
+++ ProgressPath/server/src/controllers/idea-controller.ts
@@ -1,6 +1,41 @@
 import { Request, Response } from 'express';
-import User from '../models/User';
-import IdeaPrompt from '../models/IdeaPrompt';
+import { User, IdeaPrompt, Category } from '../models/index.js';
+
+// Get all idea prompts
+export const getIdeas = async (req: Request, res: Response) => {
+  try {
+    const ideas = await IdeaPrompt.find().populate('category');
+    return res.json(ideas);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json(err);
+  }
+};
+
+// Get a single idea prompt
+export const getSingleIdeaPrompt = async (req: Request, res: Response) => {
+  try {
+    const idea = await IdeaPrompt.findById(req.params.id).populate('category');
+    if (!idea) {
+      return res.status(404).json({ message: "Idea not found!" });
+    }
+    return res.json(idea);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json(err);
+  }
+};
+
+// Get idea prompts by category
+export const getIdeaPromptsByCategory = async (req: Request, res: Response) => {
+  try {
+    const ideaPrompts = await IdeaPrompt.find({ category: req.params.categoryId }).populate('category');
+    return res.json(ideaPrompts);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json(err);
+  }
+};
 
 // Save an idea prompt (add to user's saved ideas)
 export const saveIdea = async (req: Request, res: Response) => {
@@ -80,5 +115,3 @@ export const skipIdea = async (req: Request, res: Response) => {
     res.status(500).json(err);
   }
 };
-
-
