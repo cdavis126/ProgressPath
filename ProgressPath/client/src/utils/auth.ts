6f0a52d7ef0daede6e1cbb185117150a69f02867
Celeste Hayes <celestehayes@Mac.attlocal.net>
2025-02-25 15:41:01 -0500

    Refactor auth.ts file to update the JWT interface

--- ProgressPath/client/src/utils/auth.ts
+++ ProgressPath/client/src/utils/auth.ts
@@ -2,8 +2,7 @@ import { type JwtPayload, jwtDecode } from 'jwt-decode';
 
 interface ExtendedJwt extends JwtPayload {
   data:{
-    firstName:string,
-    lastName:string,
+    username:string,
     email:string,
     id:string
   }
@@ -13,13 +12,16 @@ class AuthService {
   getProfile() {
     return jwtDecode<ExtendedJwt>(this.getToken());
   }
+
   loggedIn() {
     const token = this.getToken();
     return !!token && !this.isTokenExpired(token);
   }
+
   isTokenExpired(token: string) {
     try {
       const decoded = jwtDecode<JwtPayload>(token);
+
       if (decoded?.exp && decoded?.exp < Date.now() / 1000) {
         return true;
       }
@@ -27,14 +29,17 @@ class AuthService {
       return false;
     }
   }
+
   getToken(): string {
     const loggedUser = localStorage.getItem('id_token') || '';
     return loggedUser;
   }
+
   login(idToken: string) {
     localStorage.setItem('id_token', idToken);
     window.location.assign('/');
   }
+
   logout() {
     localStorage.removeItem('id_token');
     window.location.assign('/');
